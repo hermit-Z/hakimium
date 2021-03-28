d58b88f069f8dd58c583fb20a55fceaea4b6281d
hermit zah <59492433+hermit-Z@users.noreply.github.com>
2021-03-28 22:44:02 +0200

    router & i18n not fixed

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,7 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-    >
-     <!--
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-        </div> -->
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-       <div class="ma-2" v-for="Nitem in Nitems" :key="Nitem.name" >
-          <v-icon>{{Nitem.icon}}</v-icon>
-		<router-link :to="Nitem.url" class="green--text">{{Nitem.name}}</router-link>
-       </div>
-      <v-spacer></v-spacer>
-      <!-- -->
-      <SwitchLocale/>
-      {{ $vuetify.lang.t('$vuetify.close') }}
-      <v-switch
-        class="mt-5 pa-2"
-        v-model="$vuetify.theme.dark"
-        label="Theme"
-        persistent-hint
-      ></v-switch>
-      <!-- -->
-    </v-app-bar>
+    <SwitchLocale/>
     <v-main>
-      <HelloWorld/>
     </v-main>
     <footerr/>
   </v-app>
@@ -50,27 +9,19 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+//import HelloWorld from './components/HelloWorld.vue';
 import SwitchLocale from './components/langS.vue'
 import footerr from './components/footer.vue'
 export default Vue.extend({
   name: 'App',
 
   components: {
-    HelloWorld,
+    //HelloWorld,
     footerr,
     SwitchLocale,
   },
 
   data: () => ({
-               Nitems: [
-                       {name:'Home',icon: 'mdi-home' ,url: ''},  
-                       {name:'About',icon: 'mdi-book-account' ,url: ''},  
-                       {name:'Services',icon: 'mdi-cog' ,url: ''},  
-                       {name:'Product',icon: 'mdi-folder-home' ,url: ''},  
-                       {name:'Portfolio',icon: 'mdi-bank' ,url: ''},  
-                       {name:'Contact',icon: 'mdi-phone' ,url: ''},  
-                       ], 
     //
   }),
 });
--- src/components/langS.vue
+++ src/components/langS.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>
+
+    <v-app-bar
+      app
+      color="primary"
+    >
+     <!--
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+        </div> -->
+		<!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+       <div class="ma-2" v-for="Nitem in Nitems" :key="Nitem.name" >
+          <v-icon>{{Nitem.icon}}</v-icon>
+        <!-- <router-link :to="Nitem.url" class="green--text">{{Nitem.name}}</router-link> -->
+		<router-link :to="Nitem.url" class="green--text">{{Nitem.name}}</router-link>
+       </div>
+      <v-spacer></v-spacer>
+      <!-- -->
+      {{ $vuetify.lang.t('$vuetify.close') }}
+      {{this.$vuetify.lang.current}}
+      <v-switch
+        class="mt-5 pa-2"
+        v-model="$vuetify.theme.dark"
+        label="Theme"
+        persistent-hint
+      ></v-switch>
+      <!-- -->
 	<v-select
           col="1"
           v-if="StatLang===true"
@@ -12,14 +53,13 @@
           persistent-hint
           return-object
           single-line
-          full-width=false
           class="md1 xs3 lg1"
           > </v-select>
 	<v-btn icon @click="StatLang = !StatLang"  v-if="StatLang===false">
-      {{Select.loca}}
       <flag :iso="Select.flag" />
+      {{Select.loca}}
 	</v-btn>
-  </div>
+    </v-app-bar>
 </template>
 
 <script>
@@ -73,6 +113,16 @@ export default {
                   {loca: 'zhHant',name: 'Chinese (正體中文)',flag: 'CN'},
 
                   ],
+
+               Nitems: [
+                       {name:'Home',icon: 'mdi-home' ,url: '"this.$vuetify.lang.current" + "/"'},  
+                       {name:'About',icon: 'mdi-book-account' ,url: '{/About'},  
+                       {name:'Services',icon: 'mdi-cog' ,url: ''},  
+                       //{name:'Product',icon: 'mdi-folder-home' ,url: ''},  
+                       //{name:'Portfolio',icon: 'mdi-bank' ,url: ''},  
+                       {name:'Support',icon: 'mdi-folder-home' ,url: ''},  
+                       {name:'Contact',icon: 'mdi-phone' ,url: ''},  
+                       ], 
 	}
   }
 }
